f2662f8cc81c01df45fb7e2b6685e9241a8e2c2d
Robert M. Avram <am11.robert@yahoo.com>
2021-08-28 02:29:20 +0200

    Add as method

--- src/com/ravram/nemesis/model/JTrait.java
+++ src/com/ravram/nemesis/model/JTrait.java
@@ -1,8 +1,13 @@
 package com.ravram.nemesis.model;
 
+import com.ravram.nemesis.Attempt;
 import com.ravram.nemesis.JsonT;
+import com.ravram.nemesis.Read;
 
 public interface JTrait {
+    default <A> Attempt<A> as (final Read<A> f) {
+        return transform().as(f);
+    }
     JType type();
     JsonT transform();
     String encode();
